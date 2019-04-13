6ec4049d9462971e8ba60822f9b754f2590141de
Maya Chen <fangnihuang2016@gmail.com>
2019-04-13 13:19:40 -0400

    youtubefunction

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -78,3 +78,75 @@ const sp_api = {
 // const yt_api = {
 //   key:''
 // }
+
+// seacrh by keyword
+var keyword = "Muse";
+var apiKey = "AIzaSyCxI37dKTdAqPRZ2ObfcK2M7TaSXtr5F4c"
+
+var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q="+keyword+ "&type=video&videoCategoryId=10&key="+apiKey
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    
+    console.log(response)
+            console.log(response.items[0].snippet.title) 
+            console.log(response.items[0].snippet.publishedAt)
+            console.log(response.items[0].snippet.thumbnails.default.url)
+            console.log(response.items[0].id.videoId)
+
+             for (let i=0;i<response.items.length;i++){
+                var songTitle = response.items[i].snippet.title;//song title
+                var publishedAt = response.items[i].snippet.publishedAt;//publish time
+                var videoId = response.items[i].id.videoId;//videoid, which imports the video
+
+
+             }
+
+
+   
+  });
+
+  // function about search by username and channelId
+
+
+
+var userName = "muse";
+var channelId
+var apiKey = "AIzaSyCxI37dKTdAqPRZ2ObfcK2M7TaSXtr5F4c"
+  
+    var queryURL2 = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername="+userName+"&key="+apiKey;
+
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response)
+        console.log(response.items[0].id)
+        channelId = response.items[0].id;
+
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId="+channelId+"&type=video&key="+apiKey,
+            method: "GET"
+          }).then(function(response) {
+    
+            console.log(response)
+            console.log(response.items[0].snippet.title)
+            console.log(response.items[0].snippet.publishedAt)
+            console.log(response.items[0].snippet.thumbnails.default.url)
+
+
+             }
+
+
+
+            
+          });
+
+
+      });
+
+
+ 
+
